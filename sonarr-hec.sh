71fc570bec275eaf268c8d0134f5603c97b1f74f
Jon Hayward <jon@thehawyards.me>
2018-08-25 20:29:31 +0100

    Send sonnar events to Splunk via HEC

---
+++ sonarr-hec.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+exec 1> >(logger -s -t $(basename $0)) 2>&1
+
+host=""
+token=""
+
+action=$sonarr_eventtype
+title=$sonarr_series_title
+season=$sonarr_release_seasonnumber
+episode=$sonarr_release_episodenumbers
+file_size=$sonarr_release_size
+
+
+curl -k $host -H "Authorization: Splunk $token" -d '{"event":{"action":"'$action'","title":"'"$title"'","season":"'"$season"'","episode":"'"$episode"'","file size":"'"$file_size"'"}}'
