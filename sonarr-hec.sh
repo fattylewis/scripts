be3e03b9c33bb7dd5adc4ca63ffb8bafde9cfaa2
Jon Hayward <jon@thehaywards.me>
2018-08-30 08:24:56 +0100

    Differentiate between grab and download

--- sonarr-hec.sh
+++ sonarr-hec.sh
@@ -4,14 +4,22 @@ exec 1> >(logger -s -t $(basename $0)) 2>&1
 
 # Example host: https://hostname:8088/services/collector/event
 host=""
-# Token taken from Splunk install
+# HEC Token taken from Splunk install
 token=""
 
 action=$sonarr_eventtype
 title=$sonarr_series_title
-season=$sonarr_release_seasonnumber
-episode=$sonarr_release_episodenumbers
+release_season=$sonarr_release_seasonnumber
+release_episode=$sonarr_release_episodenumbers
+download_season=$sonarr_episodefile_seasonnumber
+download_episode=$sonarr_episodefile_episodenumbers
 file_size=$sonarr_release_size
 
 
-curl -k $host -H "Authorization: Splunk $token" -d '{"event":{"action":"'$action'","title":"'"$title"'","season":"'"$season"'","episode":"'"$episode"'","file size":"'"$file_size"'"}}'
+if [[ "$action" = Grab ]]; then
+    curl -k $host -H "Authorization: Splunk $token" -d '{"event":{"action":"'$action'","title":"'"$title"'","season":"'"$release_season"'","episode":"'"$release_episode"'","file size":"'"$file_size"'"}}'
+    exit 0
+elif [[ "$action" = Download ]]; then
+    curl -k $host -H "Authorization: Splunk $token" -d '{"event":{"action":"'$action'","title":"'"$title"'","season":"'"$download_season"'","episode":"'"$download_episode"'"}}'
+    exit 0
+fi
